31d17005e557c4c321efd864c6b809e1da46783b
Punjasin Punya <punjasin@gmail.com>
2019-03-21 00:54:48 +0700

    add dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 // import { renderRoutes } from 'react-router-config';
+import { configureStore } from './utils/configureStore';
+import { Provider } from 'react-redux';
+
+import { PersistGate } from 'redux-persist/integration/react';
 import Loadable from 'react-loadable';
 import './App.scss';
-
-const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
-
+const loading = () => (
+  <div className="animated fadeIn pt-3 text-center">Loading...</div>
+);
+export const { store, persistor } = configureStore();
 // Containers
 const DefaultLayout = Loadable({
   loader: () => import('./containers/DefaultLayout'),
@@ -34,18 +39,26 @@ const Page500 = Loadable({
 });
 
 class App extends Component {
-
   render() {
     return (
-      <HashRouter>
-          <Switch>
-            <Route exact path="/login" name="Login Page" component={Login} />
-            <Route exact path="/register" name="Register Page" component={Register} />
-            <Route exact path="/404" name="Page 404" component={Page404} />
-            <Route exact path="/500" name="Page 500" component={Page500} />
-            <Route path="/" name="Home" component={DefaultLayout} />
-          </Switch>
-      </HashRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <HashRouter>
+            <Switch>
+              <Route exact path="/login" name="Login Page" component={Login} />
+              <Route
+                exact
+                path="/register"
+                name="Register Page"
+                component={Register}
+              />
+              <Route exact path="/404" name="Page 404" component={Page404} />
+              <Route exact path="/500" name="Page 500" component={Page500} />
+              <Route path="/" name="Home" component={DefaultLayout} />
+            </Switch>
+          </HashRouter>
+        </PersistGate>
+      </Provider>
     );
   }
 }
