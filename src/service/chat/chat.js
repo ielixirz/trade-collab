f54d3290f5c6b7ac5479c4f5523ff971d409ffab
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2019-03-30 09:56:32 +0700

    CreateChatRoom , EditChatRoom

--- src/service/chat/chat.js
+++ src/service/chat/chat.js
@@ -2,6 +2,10 @@ import { FirebaseApp } from '../firebase'
 import { collection } from 'rxfire/firestore';
 import { map } from 'rxjs/operators';
 
+const ShipmentRefPath = (ShipmentKey) => {
+    return FirebaseApp.firestore().collection(`Shipment`).doc(ShipmentKey).collection(`ChatRoom`)
+}
+
 const ChatRoomRefPath = (ShipmentKey,ChatRoomKey) => {
     return FirebaseApp.firestore().collection(`Shipment`).doc(ShipmentKey).collection(`ChatRoom`).doc(ChatRoomKey)
 }
@@ -25,13 +29,43 @@ const ChatRoomMessageRefPathOrderByNewerTimestamp = (ShipmentKey,ChatRoomKey) =>
 }
 */
 
-export const CreateChatMessage = (ShipmentKey,ChatRoomKey,data) => {
-    return ChatRoomMessageRefPath(ShipmentKey,ChatRoomKey).add(data)
+export const CreateChatMessage = (ShipmentKey,ChatRoomKey,Data) => {
+    return ChatRoomMessageRefPath(ShipmentKey,ChatRoomKey).add(Data)
 }
 
 export const GetChatMessage = (ShipmentKey,ChatRoomKey) => {
     return collection(ChatRoomMessageRefPathOrderByNewerTimestamp(ShipmentKey,ChatRoomKey)).pipe(map(docs => docs.map(d => d.data())))
 }
+
+// Example Data CreateChatRoom
+
+/*
+{
+    ChatRoomName : "Exporter",
+    ChatRoomFileLink :
+        [ {FileName : "FileA.jpg" , FileUrl : "'https://firebasestorage.googleapis.com'" , FileCreateTimestamp : "123123124124124" , FilePath : "/Shipment/{ShipmentKey}/ShipmentFile/{FileKey}" }] ,
+    ChatRoomMember : {},
+    ChatRoomParticipleNotificationToken : [],
+    ChatRoomhareDataList :
+        ['Shipper','ShipmentDetail'],
+    ChatRoomPrivateShareDataList :
+        ['Trucking']
+}
+*/
+
+export const CreateChatRoom = (ShipmentKey,Data) => {
+    return ShipmentRefPath(ShipmentKey).add(Data)
+}
+
+export const EditChatRoom = (ShipmentKey,ChatRoomKey,Data) => {
+    return ChatRoomRefPath(ShipmentKey,ChatRoomKey).update(Data)
+}
+
+
+
+
+
+
 
 
 
