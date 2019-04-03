3c7b9df4d7042e97e66ecd98782a7b433154a897
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2019-04-03 15:03:11 +0700

    CreateShipment

--- src/service/shipment/shipment.js
+++ src/service/shipment/shipment.js
@@ -2,12 +2,40 @@ import { FirebaseApp } from '../firebase';
 import { collection } from 'rxfire/firestore';
 import { map } from 'rxjs/operators';
 
+const ShipmentRefPath = () => {
+  return FirebaseApp.firestore().collection(`Shipment`)
+}
+
 const ShipmentFileRefPath = (ShipmentKey) => {
     return FirebaseApp.firestore()
       .collection(`Shipment`)
       .doc(ShipmentKey)
       .collection(`ShipmentFile`);
   };
+
+/* ex. CreateShipment
+  {
+        ShipmentReference (array<object>)
+          [{ "RefOwner" : "Seller" , "RefID" : "Ref1234" , "RefTimestampUpdate" : "1234567673242"  }]
+        ShipmentSellerCompanyName (string)
+        ShipmentSourceLocation (string)
+        ShipmentBuyerCompanyName (string)
+        ShipmentDestinationLocation (string)
+        ShipmentProductName (string)
+        ShipmentETD (timestamp)
+        ShipmentETAPort (timestamp)
+        ShipmentETAWarehouse (timestamp)
+        ShipmentStatus (string)
+        ShipmentPriceDescription (string)
+        ShipmentCreatorType (string) *Importer or Exporter
+        ShipmentCreatorUserKey (string)
+        ShipmentCreateTimestamp (timestamp)
+  }
+*/
+
+export const CreateShipment = (Data) => {
+  return ShipmentRefPath().add(Data)
+}
 
 /* Example data CreateShipmentFile
   {
