38810c81a84ece2b5c3f6cc5c7b088b38afaf74e
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2019-04-11 02:59:13 +0700

    user

--- src/service/user/user.js
+++ src/service/user/user.js
@@ -1,9 +1,10 @@
-import { FirebaseApp } from '../firebase';
 import { collection, doc } from 'rxfire/firestore';
-import { from } from 'rxjs'
+import { from } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-const UserInfoRefPath = () => FirebaseApp.firestore().collection(`UserInfo`)
+import { FirebaseApp } from '../firebase';
+
+const UserInfoRefPath = () => FirebaseApp.firestore().collection('UserInfo');
 
 /*
     {
@@ -19,18 +20,28 @@ const UserInfoRefPath = () => FirebaseApp.firestore().collection(`UserInfo`)
     }
 */
 
-export const CreateUserInfo = (UserInfoKey, Data) => from(UserInfoRefPath().doc(UserInfoKey).set(Data))
+export const CreateUserInfo = (UserInfoKey, Data) => from(
+  UserInfoRefPath()
+    .doc(UserInfoKey)
+    .set(Data),
+);
 
-export const UpdateUserInfo = (UserInfoKey, Data) => from(UserInfoRefPath().doc(UserInfoKey).set(Data,{merge:true}))
+export const UpdateUserInfo = (UserInfoKey, Data) => from(
+  UserInfoRefPath()
+    .doc(UserInfoKey)
+    .set(Data, { merge: true }),
+);
 
-export const GetUserInfoDetail = UserInfoKey => doc(UserInfoRefPath().doc(UserInfoKey))
+export const GetUserInfoDetail = UserInfoKey => doc(UserInfoRefPath().doc(UserInfoKey));
 
-export const GetUserInfoFromEmail = Email => collection(UserInfoRefPath().where('UserInfoEmail','==',Email))
+export const GetUserInfoFromEmail = Email => collection(UserInfoRefPath().where('UserInfoEmail', '==', Email));
 
-export const GetUserInfoFromUsername = Username => collection(UserInfoRefPath().where('UserInfoUsername','==',Username))
+export const GetUserInfoFromUsername = Username => collection(UserInfoRefPath().where('UserInfoUsername', '==', Username));
 
-export const GetUserInfoUsername = UserInfoKey => doc(UserInfoRefPath().doc(UserInfoKey)).pipe(map(User => User.UserInfoUsername))
+export const GetUserInfoUsername = UserInfoKey => doc(
+  UserInfoRefPath().doc(UserInfoKey),
+).pipe(map(User => User.UserInfoUsername));
 
-export const GetUserInfoUserInfoNotificationToken = UserInfoKey => doc(UserInfoRefPath().doc(UserInfoKey)).pipe(map(User => User.UserInfoNotificationToken))
-
-  
+export const GetUserInfoUserInfoNotificationToken = UserInfoKey => doc(
+  UserInfoRefPath().doc(UserInfoKey),
+).pipe(map(User => User.UserInfoNotificationToken));
