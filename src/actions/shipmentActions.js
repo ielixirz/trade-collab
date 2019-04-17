5815606a4d41a6e4218e755e04e1edafed4e8a07
Punjasin Punya <punjasin@simyasolutions.com>
2019-04-17 20:21:20 +0700

    add unsubscribed

--- src/actions/shipmentActions.js
+++ src/actions/shipmentActions.js
@@ -1,25 +1,29 @@
-import _ from 'lodash';
-import { FETCH_SHIPMENT, FETCH_SHIPMENT_LIST } from '../constants/constants';
+import { FETCH_SHIPMENT_LIST } from '../constants/constants';
 import { GetShipmentList } from '../service/shipment/shipment';
+import _ from 'lodash';
 
-export const fetchShipments = (typeStatus:any) => (dispatch) => {
+let shipmentsObservable = GetShipmentList('', '', 'asc').subscribe();
+export const fetchShipments = (typeStatus: any) => dispatch => {
   let shipments = [];
-  GetShipmentList(typeStatus, '', 'asc').subscribe({
-    next: (res) => {
-      shipments = _.map(res, item => ({
-        uid: item.id,
-        ...item.data(),
-      }));
+  shipmentsObservable.unsubscribe();
+  shipmentsObservable = GetShipmentList(typeStatus, '', 'asc').subscribe({
+    next: res => {
+      shipments = _.map(res, item => {
+        return {
+          uid: item.id,
+          ...item.data()
+        };
+      });
       dispatch({
         type: FETCH_SHIPMENT_LIST,
-        payload: shipments,
+        payload: shipments
       });
       console.log(shipments);
     },
-    error: (err) => {
+    error: err => {
       console.log(err);
     },
-    complete: () => {},
+    complete: () => {}
   });
 };
 export const test = () => null;
