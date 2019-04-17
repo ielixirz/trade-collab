21ca07f65a13efaea8da4a94361c4df9d7d7218e
Teddy <shootingstarblackvirus@gmail.com>
2019-04-17 19:38:31 +0700

    fix ui

--- src/views/Shipment/Shipment.js
+++ src/views/Shipment/Shipment.js
@@ -8,11 +8,11 @@ import {
   Container,
   Button,
   Row,
-  Col
+  Col,
 } from 'reactstrap';
 import classnames from 'classnames';
-import TableShipment from './TableShipment';
 import { connect } from 'react-redux';
+import TableShipment from './TableShipment';
 import { fetchShipments } from '../../actions/shipmentActions';
 
 class Shipment extends Component {
@@ -22,21 +22,21 @@ class Shipment extends Component {
     this.toggle = this.toggle.bind(this);
     this.state = {
       activeTab: '1',
-	  typeShipment: ''
+      typeShipment: '',
     };
   }
 
   componentDidUpdate(prevProps, prevState) {
-	   if(prevState.activeTab !== this.state.activeTab) {
-		console.log('fetch shipment');
-		this.props.fetchShipments(this.state.typeShipment);
-	   }
+    if (prevState.activeTab !== this.state.activeTab) {
+      console.log('fetch shipment');
+      this.props.fetchShipments(this.state.typeShipment);
+    }
   }
 
   toggle(tab) {
     if (this.state.activeTab !== tab) {
       this.setState({
-        activeTab: tab
+        activeTab: tab,
       });
     }
   }
@@ -44,127 +44,144 @@ class Shipment extends Component {
   render() {
     console.log('this state is', this.props);
     return (
-        <div>
-          <Nav>
-            <NavItem>
-              <NavLink
-                className={classnames({ active: this.state.activeTab === '1'})}
-                onClick={() => {
-                  this.toggle('1');
-				  this.setState({typeShipment:''})
-                }}
-              >
-                <span style={styles.title}>Alert</span> <span style={styles.lineTab}>|</span>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                className={classnames({ active: this.state.activeTab === '2' })}
-                onClick={() => {
-                  this.toggle('2');
-				   this.setState({typeShipment:'plan'})
-                }}
-              >
-                 <span style={styles.title}>Plan</span> <span style={styles.lineTab}>|</span>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                className={classnames({ active: this.state.activeTab === '3' })}
-                onClick={() => {
-                  this.toggle('3');
-				   this.setState({typeShipment:'active'})
-                }}
-              >
-                 <span style={styles.title}>Active</span> <span style={styles.lineTab}>|</span>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                className={classnames({ active: this.state.activeTab === '4' })}
-                onClick={() => {
-                  this.toggle('4');
-				   this.setState({typeShipment:'complete'})
-                }}
-              >
-                 <span style={styles.title}>Complete</span> <span style={styles.lineTab}>|</span>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                className={classnames({ active: this.state.activeTab === '5' })}
-                onClick={() => {
-                  this.toggle('5');
-				   this.setState({typeShipment:'cancel'})
-                }}
-              >
-              <i className="icon-close"></i>   <span style={styles.title}>Cancel</span>
-              </NavLink>
-            </NavItem>
-			  <Col>
-                <Button style={{backgroundColor:'#16A085',marginTop:2,marginRight:10}}  className="float-right">
-                    <i className="fa fa-plus-circle" style={{color:'white'}}></i> <span style={{fontWeight:'bold',color:'white'}}>Create New Shipment</span>
-                </Button>
-		      </Col>
-          </Nav>
-          <TabContent activeTab={this.state.activeTab}>
-            <TabPane tabId="1">
-              <Row>
-                <Col sm="12">
-                  <TableShipment input={this.props.shipments} />
-                </Col>
-              </Row>
-            </TabPane>
-            <TabPane tabId="2">
-              <Row>
-                <Col sm="12"> <TableShipment input={this.props.shipments} /></Col>
-              </Row>
-            </TabPane>
-            <TabPane tabId="3">
-              <Row>
-                <Col sm="12">
-                   <TableShipment input={this.props.shipments} />
-                </Col>
-              </Row>
-            </TabPane>
-            <TabPane tabId="4">
-              <Row>
-                <Col sm="12">
-                    <TableShipment input={this.props.shipments} />
-                </Col>
-              </Row>
-            </TabPane>
-            <TabPane tabId="5">
-              <Row>
-                <Col sm="12">
-                    <TableShipment input={this.props.shipments} />
-                </Col>
-              </Row>
-            </TabPane>
-          </TabContent>
-        </div>
+      <div>
+        <Nav>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '1' })}
+              onClick={() => {
+                this.toggle('1');
+                this.setState({ typeShipment: '' });
+              }}
+            >
+              <span style={styles.title}>Alert</span>
+              {' '}
+              <span style={styles.lineTab}>|</span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '2' })}
+              onClick={() => {
+                this.toggle('2');
+                this.setState({ typeShipment: 'plan' });
+              }}
+            >
+              <span style={styles.title}>Plan</span>
+              {' '}
+              <span style={styles.lineTab}>|</span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '3' })}
+              onClick={() => {
+                this.toggle('3');
+                this.setState({ typeShipment: 'active' });
+              }}
+            >
+              <span style={styles.title}>Active</span>
+              {' '}
+              <span style={styles.lineTab}>|</span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '4' })}
+              onClick={() => {
+                this.toggle('4');
+                this.setState({ typeShipment: 'complete' });
+              }}
+            >
+              <span style={styles.title}>Complete</span>
+              {' '}
+              <span style={styles.lineTab}>|</span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '5' })}
+              onClick={() => {
+                this.toggle('5');
+                this.setState({ typeShipment: 'cancel' });
+              }}
+            >
+              <i className="icon-close" />
+              {' '}
+              <span style={styles.title}>Cancel</span>
+            </NavLink>
+          </NavItem>
+          <Col>
+            <Button
+              style={{ backgroundColor: '#16A085', marginTop: 2, marginRight: 10 }}
+              className="float-right"
+            >
+              <i className="fa fa-plus-circle" style={{ color: 'white' }} />
+              {' '}
+              <span style={{ fontWeight: 'bold', color: 'white' }}>Create New Shipment</span>
+            </Button>
+          </Col>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <TableShipment input={this.props.shipments} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="12">
+                {' '}
+                <TableShipment input={this.props.shipments} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="3">
+            <Row>
+              <Col sm="12">
+                <TableShipment input={this.props.shipments} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="4">
+            <Row>
+              <Col sm="12">
+                <TableShipment input={this.props.shipments} />
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="5">
+            <Row>
+              <Col sm="12">
+                <TableShipment input={this.props.shipments} />
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
     );
   }
 }
-const styles = { 
-   title:{
-       fontSize: 16,
-       fontWeight: 'bold',
-       color: '#707070'
-   },
-   lineTab:{
-	  color:'#EAEAEA',
-	  opacity: 0.8
-   }
-   }
-
-const mapStateToProps = state => {
-  return {
-    shipments: state.shipmentReducer.Shipments
-  };
+const styles = {
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#707070',
+  },
+  lineTab: {
+    color: '#EAEAEA',
+    opacity: 0.8,
+    marginLeft: 20,
+  },
 };
+
+const mapStateToProps = state => ({
+  shipments: state.shipmentReducer.Shipments,
+});
 
 export default connect(
   mapStateToProps,
-  { fetchShipments }
+  { fetchShipments },
 )(Shipment);
--- src/views/Shipment/TableShipment.js
+++ src/views/Shipment/TableShipment.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Chat.css';
 
 import _ from 'lodash';
-import { createDataTable } from '../../utils/tool';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
@@ -10,8 +9,8 @@ import paginationFactory from 'react-bootstrap-table2-paginator';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import Select from 'react-select'
-import './Chat.css';
+import Select from 'react-select';
+
 import {
   Row,
   Col,
@@ -21,10 +20,13 @@ import {
   Label,
   Input,
   PopoverBody,
-  InputGroup, InputGroupAddon,
-  InputGroupText
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
 } from 'reactstrap';
+import { createDataTable } from '../../utils/tool';
 import { EditShipment } from '../../service/shipment/shipment';
+
 const { SearchBar } = Search;
 
 export default class TableShipment extends React.Component {
@@ -37,7 +39,7 @@ export default class TableShipment extends React.Component {
         product: 'coconut',
         etd: 'date',
         eta: 'date',
-        status: ['intransit', 'planning']
+        status: ['intransit', 'planning'],
       },
       {
         ref: 'INVPT',
@@ -46,7 +48,7 @@ export default class TableShipment extends React.Component {
         product: 'coconut',
         etd: 'date',
         eta: 'date',
-        status: ['intransit', 'planning']
+        status: ['intransit', 'planning'],
       },
       {
         ref: 'INVPT',
@@ -55,7 +57,7 @@ export default class TableShipment extends React.Component {
         product: 'coconut',
         etd: 'date',
         eta: 'date',
-        status: ['intransit', 'planning']
+        status: ['intransit', 'planning'],
       },
       {
         ref: 'INVPT',
@@ -64,7 +66,7 @@ export default class TableShipment extends React.Component {
         product: 'coconut',
         etd: 'date',
         eta: 'date',
-        status: ['intransit', 'planning']
+        status: ['intransit', 'planning'],
       },
       {
         ref: 'INVPT',
@@ -73,7 +75,7 @@ export default class TableShipment extends React.Component {
         product: 'coconut',
         etd: 'date',
         eta: 'date',
-        status: ['intransit', 'planning']
+        status: ['intransit', 'planning'],
       },
       {
         ref: 'INVPT',
@@ -82,71 +84,76 @@ export default class TableShipment extends React.Component {
         product: 'coconut',
         etd: 'date',
         eta: 'date',
-        status: ['intransit', 'planning']
-      }
+        status: ['intransit', 'planning'],
+      },
     ],
     columns: [
       {
         dataField: 'ref',
-        text: 'Ref:'
+        text: 'Ref:',
       },
       {
         dataField: 'seller',
         text: 'Seller',
-        sort: true
+        sort: true,
       },
       {
         dataField: 'buyer',
         text: 'Buyer',
-        sort: true
+        sort: true,
       },
       {
         dataField: 'product',
-        text: 'Product'
+        text: 'Product',
       },
       {
         dataField: 'etd',
         text: 'ETD',
-        sort: true
+        sort: true,
       },
       {
         dataField: 'eta',
         text: 'ETA',
-        sort: true
+        sort: true,
       },
       {
         dataField: 'status',
-        text: 'Status'
-      }
-    ]
+        text: 'Status',
+      },
+    ],
   };
 
   renderRefComponent(index, ref) {
     console.log(ref);
     return (
       <div>
-        <p id={'popover' + index} >
-          {ref.RefID}
-        </p>
-        <UncontrolledPopover  trigger="legacy" placement="bottom" target={'popover' + index}>
+        <p id={`popover${index}`}>{ref.RefID}</p>
+        <UncontrolledPopover trigger="legacy" placement="bottom" target={`popover${index}`}>
           <PopoverBody>
             <Row>
               <Col xs={1} />
-              <Col xs={5} style={{ paddingTop:5}} >
+              <Col xs={5} style={{ paddingTop: 5 }}>
                 <Label check>
-                  <Input style={{paddingTop:5}}  type="radio" name={'shipmentRef' + index} value={ref.RefID} />
-                  Ref #1 : ({ref.RefOwner})
+                  <Input
+                    style={{ paddingTop: 5 }}
+                    type="radio"
+                    name={`shipmentRef${index}`}
+                    value={ref.RefID}
+                  />
+                  Ref #1 : (
+                  {ref.RefOwner}
+)
                 </Label>
               </Col>
               <Col xs={5}>
                 <Input
                   type="text"
-                  name={'shipmentRefID' + index}
-                  id={'shipmentRefID' + index}
+                  name={`shipmentRefID${index}`}
+                  id={`shipmentRefID${index}`}
                   value={ref.RefID}
                   maxlength={50}
                   disabled
-				  bsSize="sm"
+                  bsSize="sm"
                 />
               </Col>
             </Row>
@@ -161,10 +168,10 @@ export default class TableShipment extends React.Component {
       <div>
         <Input
           type="select"
-          onChange={e => {
+          onChange={(e) => {
             const value = e.target.value;
             EditShipment(item.uid, {
-              ShipmentStatus: value
+              ShipmentStatus: value,
             });
           }}
         >
@@ -205,8 +212,8 @@ export default class TableShipment extends React.Component {
       columns = input.columns;
     } else {
       input = _.map(this.props.input, (item, index) => {
-        let etd = _.get(item, 'ShipmentETD', 0);
-        let eta = _.get(item, 'ShipmentETAPort', 0);
+        const etd = _.get(item, 'ShipmentETD', 0);
+        const eta = _.get(item, 'ShipmentETAPort', 0);
 
         return {
           Ref: this.renderRefComponent(index, _.get(item, 'ShipmentReference', 'input your Ref')),
@@ -215,125 +222,125 @@ export default class TableShipment extends React.Component {
           Product: _.get(item, 'ShipmentProductName', ''),
           ETD: new Date(etd.seconds * 1000).toLocaleString(),
           ETA: new Date(eta.seconds * 1000).toLocaleString(),
-          Status: this.renderStatusComponent(item)
+          Status: this.renderStatusComponent(item),
         };
       });
       input = createDataTable(input);
       data = input.data;
       columns = input.columns;
     }
-	const selectRow = {
-		  mode: 'checkbox',
-		  clickToSelect: true,
-		  hideSelectColumn: true,
-		  bgColor: '#F5FBFA'
-		};
-		const pageListRenderer = ({
-		  pages,
-		  onPageChange
-		}) => {
-		  const pageWithoutIndication = pages.filter(p => typeof p.page !== 'string');
-		  return (
-			<div>
-			  <span>Page </span>
-			  {
-				pageWithoutIndication.map(p => (
-				 <Button outline color="secondary" onClick={ () => onPageChange(p.page) }>{ p.page }</Button>
-				))
-			  }
-			</div>
-		  );
-		};
-        const sizePerPageRenderer = ({
-			  options,
-			  currSizePerPage,
-			  onSizePerPageChange
-			}) => (
-			  <div className="btn-group" role="group">
-				{
-				  options.map((option) => {
-					const isSelect = currSizePerPage === `${option.page}`;
-					return (
-					  <button
-						key={ option.text }
-						type="button"
-						onClick={ () => onSizePerPageChange(option.page) }
-						className={ `btn ${isSelect ? 'btn btn-light' : 'btn btn-dark'}` }
-					  >
-						{ option.text }
-					  </button>
-					);
-				  })
-				}
-			  </div>
-		);
-		const options = {
-		  pageListRenderer,
-		  sizePerPageRenderer
-		};
-		const MySearch = (props) => {
-			  let input;
-			  const handleClick = () => {
-				props.onSearch(input.value);
-			  };
-			  return (
-				<div>
-				 <InputGroup>
-				 <InputGroupAddon addonType="prepend"><InputGroupText style={{backgroundColor:'white'}}><i className="fa fa-filter"/></InputGroupText></InputGroupAddon>
-		         <Input
-					className="form-control"
-					id="statusSelect"
-					ref={ n => input = n }
-					name="select"
-					type="select"
-					style={{width:170}}
-					onChange={handleClick}
-				  >
-				   <option>Intransit</option>
-					<option>Order</option>
-					<option>Delayed</option>
-					<option>Cancelled</option>
-					<option>Delivered</option>
-				  </Input>
-				 </InputGroup>
-				</div>
-			  );
-			};
+    const selectRow = {
+      mode: 'checkbox',
+      clickToSelect: true,
+      hideSelectColumn: true,
+      bgColor: '#F5FBFA',
+    };
+    const pageListRenderer = ({ pages, onPageChange }) => {
+      const pageWithoutIndication = pages.filter(p => typeof p.page !== 'string');
+      return (
+        <div>
+          <span>Page </span>
+          {pageWithoutIndication.map(p => (
+            <Button outline color="secondary" onClick={() => onPageChange(p.page)}>
+              {p.page}
+            </Button>
+          ))}
+        </div>
+      );
+    };
+    const sizePerPageRenderer = ({ options, currSizePerPage, onSizePerPageChange }) => (
+      <div className="btn-group" role="group">
+        {options.map((option) => {
+          const isSelect = currSizePerPage === `${option.page}`;
+          return (
+            <button
+              key={option.text}
+              type="button"
+              onClick={() => onSizePerPageChange(option.page)}
+              className={`btn ${isSelect ? 'btn btn-light' : 'btn btn-dark'}`}
+            >
+              {option.text}
+            </button>
+          );
+        })}
+      </div>
+    );
+    const options = {
+      pageListRenderer,
+      sizePerPageRenderer,
+    };
+    const MySearch = (props) => {
+      let input;
+      const handleClick = () => {
+        props.onSearch(input.value);
+      };
+      return (
+        <div>
+          <InputGroup>
+            <InputGroupAddon addonType="prepend">
+              <InputGroupText style={{ backgroundColor: 'white' }}>
+                <i className="fa fa-filter" />
+              </InputGroupText>
+            </InputGroupAddon>
+            <Input
+              className="form-control"
+              id="statusSelect"
+              ref={n => (input = n)}
+              name="select"
+              type="select"
+              style={{ width: 170 }}
+              onChange={handleClick}
+            >
+              <option>Intransit</option>
+              <option>Order</option>
+              <option>Delayed</option>
+              <option>Cancelled</option>
+              <option>Delivered</option>
+            </Input>
+          </InputGroup>
+        </div>
+      );
+    };
     return (
-        <ToolkitProvider keyField="id" data={data} columns={columns} search>
-          {props => (
-            <div>
-              <Row>
-                <Col xs="2">
-                  <SearchBar {...props.searchProps} placeholder="&#xF002; Typing" id="search" style={{width:250,height:38}} />
-                </Col>
-				<Col xs="2">
-				  <MySearch { ...props.searchProps } />
-				</Col>
-				<Col xs="6"/>
-				<Col xs="2" style={{ display: 'flex',justifyContent: 'flex-end',marginBottom:10}}>
-				  <Button style={{backgroundColor:'#16A085',marginTop:2,marginRight:10}}>
-                   <span style={{fontWeight:'bold',color:'white'}}>Save</span>
+      <ToolkitProvider keyField="id" data={data} columns={columns} search>
+        {props => (
+          <div>
+            <Row>
+              <Col xs="2">
+                <SearchBar
+                  {...props.searchProps}
+                  placeholder="&#xF002; Typing"
+                  id="search"
+                  style={{ width: 250, height: 38 }}
+                />
+              </Col>
+              <Col xs="2">
+                <MySearch {...props.searchProps} />
+              </Col>
+              <Col xs="6" />
+              <Col xs="2" style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: 10 }}>
+                <Button style={{ backgroundColor: '#16A085', marginTop: 2, marginRight: 10 }}>
+                  <span style={{ fontWeight: 'bold', color: 'white' }}>Save</span>
                 </Button>
-				  <Button style={{backgroundColor:'white',marginTop:2,marginRight:10}} >
-                    <i className="icons cui-pencil" style={{color:'black'}}></i> <span style={{fontWeight:'bold',color:'#707070'}}>Edit</span>
+                <Button style={{ backgroundColor: 'white', marginTop: 2, marginRight: 10 }}>
+                  <i className="icons cui-pencil" style={{ color: 'black' }} />
+                  {' '}
+                  <span style={{ fontWeight: 'bold', color: '#707070' }}>Edit</span>
                 </Button>
-				</Col>
-              </Row>
-			  <div className='table'>
+              </Col>
+            </Row>
+            <div className="table">
               <BootstrapTable
                 rowStyle={{ textAlign: 'left' }}
                 {...props.baseProps}
                 pagination={paginationFactory(options)}
-				bordered={ false }
-				selectRow={ selectRow }
-				wrapperClasses="boo"
+                bordered={false}
+                wrapperClasses="boo"
               />
-			  </div>
             </div>
-          )}
-        </ToolkitProvider>
- 
+          </div>
+        )}
+      </ToolkitProvider>
     );
   }
 }
