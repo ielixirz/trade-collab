90b054eb8f5f3ff44afa66504cef66de77595f88
Rootrapee <r.rattanapipit@gmail.com>
2019-04-18 17:54:28 +0700

    lint run fix

--- src/actions/shipmentActions.js
+++ src/actions/shipmentActions.js
@@ -1,29 +1,27 @@
+import _ from 'lodash';
 import { FETCH_SHIPMENT_LIST } from '../constants/constants';
 import { GetShipmentList } from '../service/shipment/shipment';
-import _ from 'lodash';
 
 let shipmentsObservable = GetShipmentList('', '', 'asc').subscribe();
-export const fetchShipments = (typeStatus: any) => dispatch => {
+export const fetchShipments = (typeStatus: any) => (dispatch) => {
   let shipments = [];
   shipmentsObservable.unsubscribe();
   shipmentsObservable = GetShipmentList(typeStatus, '', 'asc').subscribe({
-    next: res => {
-      shipments = _.map(res, item => {
-        return {
-          uid: item.id,
-          ...item.data()
-        };
-      });
+    next: (res) => {
+      shipments = _.map(res, item => ({
+        uid: item.id,
+        ...item.data(),
+      }));
       dispatch({
         type: FETCH_SHIPMENT_LIST,
-        payload: shipments
+        payload: shipments,
       });
       console.log(shipments);
     },
-    error: err => {
+    error: (err) => {
       console.log(err);
     },
-    complete: () => {}
+    complete: () => {},
   });
 };
 export const test = () => null;
