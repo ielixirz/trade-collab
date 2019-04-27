c2bea74871444a7a1c373ef07e7da86cef07963c
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2019-04-28 01:18:53 +0700

    UpdateProfile

--- src/service/user/profile.js
+++ src/service/user/profile.js
@@ -2,7 +2,10 @@ import { collection, doc } from 'rxfire/firestore';
 import { from } from 'rxjs';
 import { FirebaseApp } from '../firebase';
 
-const ProfileRefPath = UserInfoKey => FirebaseApp.firestore().collection('UserInfo').doc(UserInfoKey).collection('Profile');
+const ProfileRefPath = UserInfoKey => FirebaseApp.firestore()
+  .collection('UserInfo')
+  .doc(UserInfoKey)
+  .collection('Profile');
 
 /*
  {
@@ -19,6 +22,11 @@ export const CreateProfile = (UserInfoKey, Data) => from(ProfileRefPath(UserInfo
 
 export const GetProlfileList = UserInfoKey => collection(ProfileRefPath(UserInfoKey));
 
-export const GetProfileDetail = (UserInfoKey, ProfileKey) => doc(
-  ProfileRefPath(UserInfoKey).doc(ProfileKey),
+// eslint-disable-next-line max-len
+export const GetProfileDetail = (UserInfoKey, ProfileKey) => doc(ProfileRefPath(UserInfoKey).doc(ProfileKey));
+
+export const UpdateProfile = (UserInfoKey, ProfileKey, Data) => from(
+  ProfileRefPath(UserInfoKey)
+    .doc(ProfileKey)
+    .set(Data, { merge: true }),
 );
