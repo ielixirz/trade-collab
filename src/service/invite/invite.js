b25adca4899cc00fa759ae9c69c87e3a87e118ff
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2019-05-08 02:17:40 +0700

    CompanyInvitationRefPath

---
+++ src/service/invite/invite.js
@@ -0,0 +1,10 @@
+import { collection, doc } from 'rxfire/firestore';
+import { from } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+import { FirebaseApp } from '../firebase';
+
+const CompanyInvitationRefPath = CompanyKey => FirebaseApp.firestore()
+  .collection('Company')
+  .doc(CompanyKey)
+  .collection('CompanyInvitation');
