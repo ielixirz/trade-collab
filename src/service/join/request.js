4ba7a40a7c6ef5d63119ed1f0e9bef19a510266d
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2019-05-08 02:24:07 +0700

    CompanyRequestRefPath

---
+++ src/service/join/request.js
@@ -0,0 +1,10 @@
+import { collection, doc } from 'rxfire/firestore';
+import { from } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+import { FirebaseApp } from '../firebase';
+
+const CompanyRequestRefPath = CompanyKey => FirebaseApp.firestore()
+  .collection('Company')
+  .doc(CompanyKey)
+  .collection('CompanyRequest');
