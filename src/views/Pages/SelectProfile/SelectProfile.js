47343c2482ce5ec897e8f83a968f770b5a223a31
Teddy <shootingstarblackvirus@gmail.com>
2019-05-07 09:01:28 +0700

    finish select profile ui

---
+++ src/views/Pages/SelectProfile/SelectProfile.js
@@ -0,0 +1,69 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable filenames/match-regex */
+import React, { Component } from 'react';
+import { Col, Container, Row } from 'reactstrap';
+import './card.css';
+
+const styles = {};
+
+class SelectProfile extends Component {
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="9" lg="7" xl="6">
+              <div className="card">
+                <div className="container">
+                  <h2>Select Profile</h2>
+                  <div className="cardSelect">
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        flex: 1,
+                      }}
+                    >
+                      <img
+                        src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/46491975_2461706807202990_8323584076334235648_n.jpg?_nc_cat=103&_nc_eui2=AeGn67viBaYidmrn_yWeMaX6AMBuov3nYmk0a3llhOLDSXB7ZkYqVOfdhs4ccrLKPCWRey-jjR1i4_oRS7MShnX_GA8CHblXrqXXsouL8Cb7yA&_nc_ht=scontent.fbkk22-2.fna&oh=c2ace08edac9ee9b9fda8bb7632da8c1&oe=5D6A9D95"
+                        alt="Avatar"
+                        style={{
+                          display: 'flex',
+                          width: 50,
+                          height: 50,
+                          borderRadius: 50 / 2,
+                          marginLeft: 10,
+                          flex: 0.2,
+                        }}
+                      />
+                      <h4
+                        style={{
+                          display: 'flex',
+                          marginLeft: 20,
+                          textAlign: 'center',
+                          flex: 0.8,
+                        }}
+                      >
+                        Rootrapee
+                      </h4>
+                    </div>
+                  </div>
+                  <div style={{ textAlign: 'center' }}>
+                    <p style={{ color: '#16a085' }}>Add New User</p>
+                    <p style={{ color: '#16a085' }}>
+                      You can have multiple users using the same e-mail address
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default SelectProfile;
