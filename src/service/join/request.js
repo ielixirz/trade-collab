9b7541d5576bb97cd887e664c8e2bd5c37cb4ac7
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2019-05-08 03:20:46 +0700

    CreateCompanyRequest

--- src/service/join/request.js
+++ src/service/join/request.js
@@ -13,3 +13,27 @@ const UserRequestRefPath = UserKey => FirebaseApp.firestore()
   .collection('User')
   .doc(UserKey)
   .collection('UserRequest');
+
+/* CreateCompanyRequest
+    {
+        UserRequestReference (reference)
+        UserRequestKey (reference)
+        UserRequestUserKey (string)
+        UserRequestCompanyKey (string)
+        UserRequestFristname (string)
+        UserRequestSurname (string)
+        UserRequestEmail (string)
+        UserRequestNote (string)
+        UserRequestTimestamp (timestamp)
+        UserInvitationStatus (string) ('Reject','Approve','Pending')
+    }
+*/
+
+// eslint-disable-next-line max-len
+export const CreateCompanyRequest = (CompanyKey, RequestKey, Data) => from(
+  collection(
+    CompanyRequestRefPath(CompanyKey)
+      .doc(RequestKey)
+      .set(Data),
+  ),
+);
