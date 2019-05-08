71de1ca52204817ece576e971add2f2ab9a86c6e
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2019-05-09 04:18:22 +0700

    user to UserInfo

--- src/service/join/request.js
+++ src/service/join/request.js
@@ -10,7 +10,7 @@ const CompanyRequestRefPath = CompanyKey => FirebaseApp.firestore()
   .collection('CompanyRequest');
 
 const UserRequestRefPath = UserKey => FirebaseApp.firestore()
-  .collection('User')
+  .collection('UserInfo')
   .doc(UserKey)
   .collection('UserRequest');
 
@@ -82,3 +82,123 @@ export const DeleteUserRequest = (UserKey, RequestKey) => from(
       .delete(),
   ),
 );
+
+export const CreateMultipleInvitation = (ColleaguesDataList, CompanyKey) => {
+  const EmailList = ColleaguesDataList.map(ColleaguesItem => ColleaguesItem.Email);
+  const RoleList = ColleaguesDataList.map(ColleaguesItem => ColleaguesItem.Role);
+  const PositionList = ColleaguesDataList.map(ColleaguesItem => ColleaguesItem.Position);
+
+  const EmailListSource = from(EmailList).pipe(
+    tap(mail => mail),
+    concatMap(Email => GetUserInfoFromEmail(Email).pipe(take(1))),
+    // prettier-ignore
+    map(UserInfoList => UserInfoList.map((UserInfoDoc) => {
+      const Data = UserInfoDoc.data();
+      const ID = UserInfoDoc.id;
+      const Doc = { ...Data, id: ID };
+      return Doc;
+    })),
+    map((UserInfoDocData, index) => ({
+      UserInvitationReference: '',
+      UserInvitationUserKey: UserInfoDocData[index].id,
+      UserInvitationCompanyKey: CompanyKey,
+      // UserInvitationFirstname: UserInfoDocData[index].UserInfoFirstname,
+      // UserInvitationSurname: UserInfoDocData[index].UserInfoSurname,
+      UserInvitationFirstname: '',
+      UserInvitationSurname: '',
+      UserInvitationEmail: UserInfoDocData[index].UserInfoEmail,
+      UserInvitationPosition: ColleaguesDataList[index].Position,
+      UserInvitationRole: ColleaguesDataList[index].Role,
+      UserInvitationTimestamp: new Date(),
+      UserInvitationStatus: 'Pending',
+    })),
+  );
+
+  // const CompanySource = GetCompanyDetail(CompanyKey).pipe(
+  //   map(CompanyDoc => CompanyDoc.data()),
+  //   map(CompanyDocData => RoleList.map((RoleItem, Index) => {
+  //     const payload = {
+  //       CompanyInvitationReference: '',
+  //       CompanyInvitationCompanyKey: CompanyKey,
+  //       CompanyInvitationName: CompanyDocData.CompanyName,
+  //       // CompanyInvitationEmail: CompanyDocData.CompanyEmail,
+  //       CompanyInvitationEmail: '',
+  //       CompanyInvitationPosition: PositionList[Index],
+  //       CompanyInvitationRole: RoleItem,
+  //       CompanyInvitationTimestamp: new Date(),
+  //       CompanyInvitationStatus: 'Pending',
+  //     };
+  //     return payload;
+  //   })),
+  //   take(1),
+  // );
+
+  const CompanySource = Email => GetCompanyDetail(CompanyKey).pipe(
+    map(CompanyDoc => CompanyDoc.data()),
+    map((CompanyDocData) => {
+      const PreloadData = _.find(ColleaguesDataList, { Email });
+      const payload = {
+        CompanyInvitationReference: '',
+        CompanyInvitationCompanyKey: CompanyKey,
+        CompanyInvitationName: CompanyDocData.CompanyName,
+        // CompanyInvitationEmail: CompanyDocData.CompanyEmail,
+        CompanyInvitationEmail: '',
+        CompanyInvitationPosition: PreloadData.Position,
+        CompanyInvitationRole: PreloadData.Role,
+        CompanyInvitationTimestamp: new Date(),
+        CompanyInvitationStatus: 'Pending',
+      };
+
+      return payload;
+    }),
+    take(1),
+  );
+
+  return EmailListSource.pipe(
+    concatMap((UserInvitationPayload) => {
+      console.log(UserInvitationPayload);
+      return combineLatest(
+        CreateCompanyInvitation(CompanyKey, UserInvitationPayload),
+        CompanySource(UserInvitationPayload.UserInvitationEmail),
+        of(UserInvitationPayload.UserInvitationUserKey),
+      );
+    }),
+    concatMap(res => CreateUserInvitation(res[2], res[0].id, res[1])),
+  );
+  // return of(EmailListSource, CompanySource).pipe(concatAll());
+
+  // return combineLatest(EmailListSource, CompanySource).pipe(
+  //   map(r => r[0].map((item, index) => ({ UserInvitation: item, CompanyInvitation: r[1][index] }))),
+  //   switchMap(indexing => indexing.map((item) => {
+  //     const InvitationKey = CreateCompanyInvitation(CompanyKey, item.UserInvitation)
+  //       .pipe(map(res => res.id))
+  //       .subscribe();
+
+  //     return InvitationKey;
+  //   })),
+  // );
+
+  // return concat(EmailListSource, CompanySource).pipe(toArray());
+
+  // const CombineSource = combineLatest(EmailListSource, CompanySource);
+
+  // const UserKey = CombineSource.pipe(
+  //   map(Combine => Combine[0].map(item => item.UserInvitationUserKey)),
+  // );
+
+  // const InviteKey = CombineSource.pipe(
+  //   map(Combine => Combine[1]),
+  //   // concatMap(CombineItem => combineLatest(CombineItem)),
+  //   // tap(a => console.log(a)),
+  //   concatMap(DataItem => DataItem),
+
+  //   // switchMap(UserInvitationData => CreateCompanyInvitation(CompanyKey, UserInvitationData)),
+  //   // map(Response => Response.id),
+  // );
+
+  // return InviteKey;
+
+  // return forkJoin(UserKey, InviteKey).pipe(
+
+  // );
+};
