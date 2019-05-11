bab758aba9a7cede541e7116f9355bbd39c14f95
Rootrapee <r.rattanapipit@gmail.com>
2019-05-12 01:17:38 +0700

    revise name

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,5 +1,5 @@
-const EMAIL_REGEX = RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$");
-export const isValidEmail = email => EMAIL_REGEX.test(email);
+const VALID_EMAIL_REGEX = RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$");
+export const isValidEmail = email => VALID_EMAIL_REGEX.test(email);
 
 const VALID_NAME_REGEX = RegExp('\\(|!|@|\\*|&|%');
 export const isValidName = name => !VALID_NAME_REGEX.test(name);
