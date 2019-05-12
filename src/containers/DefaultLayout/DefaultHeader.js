c7d6b7f95b73b7a3a39a450d39ed3423e53fccd6
Punjasin Punya <punjasin@gmail.com>
2019-05-12 22:28:37 +0700

    Complete Notification Modal

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -9,7 +9,9 @@ import {
   DropdownToggle,
   Nav,
   NavItem,
-  UncontrolledDropdown
+  UncontrolledDropdown,
+  Row,
+  Col
 } from 'reactstrap';
 import PropTypes from 'prop-types';
 
@@ -28,6 +30,42 @@ const defaultProps = {};
 
 class DefaultHeader extends Component {
   render() {
+    let notification = [
+      {
+        id: 1,
+        text: 'Holy.Wisdom has request to join Liam Produce',
+        time: new Date(),
+        isRead: false,
+        sender: 'John',
+        type: 'Shipment',
+        avatar:
+          'https://firebasestorage.googleapis.com/v0/b/yterminal-b0906.appspot.com/o/Profile%2F2sYaYykLYOd5a2D4FPbV%2F1556806598505Screen%20Shot%202562-04-29%20at%2023.54.33.png?alt=media&token=eda3cf62-d247-45dd-97c5-e7c9022a00bb',
+        ShipmentKey: 'i5SXjeRcWWLMGd9m4bcg'
+      },
+      {
+        id: 2,
+        text: 'Pooh.Elixir has been invite to join Importer Chatroom',
+        time: new Date(),
+        isRead: true,
+        sender: 'Pooh',
+        type: 'Chat',
+        avatar:
+          'https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/46491975_2461706807202990_8323584076334235648_n.jpg?_nc_cat=103&_nc_eui2=AeGn67viBaYidmrn_yWeMaX6AMBuov3nYmk0a3llhOLDSXB7ZkYqVOfdhs4ccrLKPCWRey-jjR1i4_oRS7MShnX_GA8CHblXrqXXsouL8Cb7yA&_nc_ht=scontent.fbkk22-2.fna&oh=c2ace08edac9ee9b9fda8bb7632da8c1&oe=5D6A9D95',
+
+        ShipmentKey: 'i5SXjeRcWWLMGd9m4bcg'
+      },
+      {
+        id: 3,
+        text: 'John_Lee_J.Lee_skytex.com_has_requested_to_join_Y-Terminal_Co._Ltd.',
+        time: new Date(),
+        isRead: false,
+        type: 'Shipment',
+        sender: 'Pooh',
+        avatar:
+          'https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/46491975_2461706807202990_8323584076334235648_n.jpg?_nc_cat=103&_nc_eui2=AeGn67viBaYidmrn_yWeMaX6AMBuov3nYmk0a3llhOLDSXB7ZkYqVOfdhs4ccrLKPCWRey-jjR1i4_oRS7MShnX_GA8CHblXrqXXsouL8Cb7yA&_nc_ht=scontent.fbkk22-2.fna&oh=c2ace08edac9ee9b9fda8bb7632da8c1&oe=5D6A9D95',
+        ShipmentKey: 'i5SXjeRcWWLMGd9m4bcg'
+      }
+    ];
     // eslint-disable-next-line
     const { children, ...attributes } = this.props;
     console.log('props', this.props);
@@ -101,39 +139,35 @@ class DefaultHeader extends Component {
                   </div>
                 </span>
               </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-bell-o" /> Updates<Badge color="info">42</Badge>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-envelope-o" /> Messages<Badge color="success">42</Badge>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-tasks" /> Tasks<Badge color="danger">42</Badge>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-comments" /> Comments<Badge color="warning">42</Badge>
-              </DropdownItem>
-              <DropdownItem header tag="div" className="text-center">
-                <strong>Settings</strong>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-user" /> Profile
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-wrench" /> Settings
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-usd" /> Payments<Badge color="secondary">42</Badge>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-file" /> Projects<Badge color="primary">42</Badge>
-              </DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>
-                <i className="fa fa-shield" /> Lock Account
-              </DropdownItem>
-              <DropdownItem onClick={e => this.props.onLogout(e)}>
-                <i className="fa fa-lock" /> Logout
+              {notification.map((item, index) => {
+                return (
+                  <DropdownItem
+                    key={'notification' + index}
+                    className={item.isRead ? '' : 'highlight'}
+                  >
+                    <div>
+                      <div className="message">
+                        <div className="pt-3 mr-3 float-left">
+                          <div className="avatar">
+                            <img src={item.avatar} className="img-avatar" />
+                            <span className="avatar-status badge-success" />
+                          </div>
+                        </div>
+                        <div>
+                          <small className="text-muted">{item.sender}</small>
+                          <small className="text-muted float-right mt-1">
+                            {item.time.toLocaleString()}
+                          </small>
+                        </div>
+
+                        <div className="small text-muted text-truncate">{item.text}</div>
+                      </div>
+                    </div>
+                  </DropdownItem>
+                );
+              })}
+              <DropdownItem className="text-center">
+                <Link to="/notification">See All</Link>
               </DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
--- src/views/Alert/Alert.js
+++ src/views/Alert/Alert.js
@@ -4,42 +4,91 @@ import {
   Button,
   Col,
   Container,
+  DropdownItem,
   Input,
   InputGroup,
   InputGroupAddon,
   InputGroupText,
   Row
 } from 'reactstrap';
-
+import './alert.css';
 // eslint-disable-next-line react/prefer-stateless-function
 class Alert extends Component {
   render() {
+    let notification = [
+      {
+        id: 1,
+        text: 'Holy.Wisdom has request to join Liam Produce',
+        time: new Date(),
+        isRead: false,
+        sender: 'John',
+        type: 'Shipment',
+        avatar:
+          'https://firebasestorage.googleapis.com/v0/b/yterminal-b0906.appspot.com/o/Profile%2F2sYaYykLYOd5a2D4FPbV%2F1556806598505Screen%20Shot%202562-04-29%20at%2023.54.33.png?alt=media&token=eda3cf62-d247-45dd-97c5-e7c9022a00bb',
+        ShipmentKey: 'i5SXjeRcWWLMGd9m4bcg'
+      },
+      {
+        id: 2,
+        text: 'Pooh.Elixir has been invite to join Importer Chatroom',
+        time: new Date(),
+        isRead: true,
+        sender: 'Pooh',
+        type: 'Chat',
+        avatar:
+          'https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/46491975_2461706807202990_8323584076334235648_n.jpg?_nc_cat=103&_nc_eui2=AeGn67viBaYidmrn_yWeMaX6AMBuov3nYmk0a3llhOLDSXB7ZkYqVOfdhs4ccrLKPCWRey-jjR1i4_oRS7MShnX_GA8CHblXrqXXsouL8Cb7yA&_nc_ht=scontent.fbkk22-2.fna&oh=c2ace08edac9ee9b9fda8bb7632da8c1&oe=5D6A9D95',
+
+        ShipmentKey: 'i5SXjeRcWWLMGd9m4bcg'
+      },
+      {
+        id: 3,
+        text: 'John_Lee_J.Lee_skytex.com_has_requested_to_join_Y-Terminal_Co._Ltd.',
+        time: new Date(),
+        isRead: false,
+        type: 'Shipment',
+        sender: 'Pooh',
+        avatar:
+          'https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/46491975_2461706807202990_8323584076334235648_n.jpg?_nc_cat=103&_nc_eui2=AeGn67viBaYidmrn_yWeMaX6AMBuov3nYmk0a3llhOLDSXB7ZkYqVOfdhs4ccrLKPCWRey-jjR1i4_oRS7MShnX_GA8CHblXrqXXsouL8Cb7yA&_nc_ht=scontent.fbkk22-2.fna&oh=c2ace08edac9ee9b9fda8bb7632da8c1&oe=5D6A9D95',
+        ShipmentKey: 'i5SXjeRcWWLMGd9m4bcg'
+      }
+    ];
     return (
-      <div className="app flex-row align-items-center">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="6">
-              <span className="clearfix">
-                <h1 className="float-left display-3 mr-4">500</h1>
-                <h4 className="pt-3">Houston, we have a problem!</h4>
-                <p className="text-muted float-left">
-                  The page you are looking for is temporarily unavailable.
-                </p>
-              </span>
-              <InputGroup className="input-prepend">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    <i className="fa fa-search" />
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input size="16" type="text" placeholder="What are you looking for?" />
-                <InputGroupAddon addonType="append">
-                  <Button color="info">Search</Button>
-                </InputGroupAddon>
-              </InputGroup>
-            </Col>
-          </Row>
-        </Container>
+      <div
+        className="animated fadeIn"
+        style={{
+          marginTop: '10vh'
+        }}
+      >
+        {notification.map((item, index) => {
+          return (
+            <div className="container">
+              <a
+                href="#"
+                onClick={e => {
+                  e.preventDefault();
+                }}
+              >
+                <Row className="notification">
+                  <div className={item.isRead ? 'notificationItem' : 'notificationItem highlight'}>
+                    <div className="pt-3 mr-3 float-left">
+                      <div className="avatar">
+                        <img src={item.avatar} className="img-avatar" />
+                        <span className="avatar-status badge-success" />
+                      </div>
+                    </div>
+                    <div>
+                      <small className="text-muted">{item.sender}</small>
+                      <small className="text-muted float-right mt-1">
+                        {item.time.toLocaleString()}
+                      </small>
+                    </div>
+
+                    <div className="small text-muted text-truncate">{item.text}</div>
+                  </div>
+                </Row>
+              </a>
+            </div>
+          );
+        })}
       </div>
     );
   }
