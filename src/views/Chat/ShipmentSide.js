c41acd5b7c0932e1038ea2a2de15b0442bf28506
Rootrapee <r.rattanapipit@gmail.com>
2019-05-16 00:36:38 +0700

    reformat ShipmentSide

--- src/views/Chat/ShipmentSide.js
+++ src/views/Chat/ShipmentSide.js
@@ -1,83 +1,95 @@
-import React, { Component,useContext,useReducer } from 'react';
+/* eslint-disable filenames/match-regex */
+import React, { Component, useContext, useReducer } from 'react';
 
 import {
-    Collapse, CardBody, Card, Row, Col
-  } from 'reactstrap';
+  Collapse, CardBody, Card, Row, Col,
+} from 'reactstrap';
 import shipmentListContext from '../../context/shipmentContext';
 import shipmentReducer from '../../reducers/shipmentReducer';
 import ShipmentList from '../../component/ShipmentList';
- 
-const ShipmentData = ()=>{
-    const initialState =  useContext(shipmentListContext);
-    const [state,dispatch]  = useReducer(shipmentReducer,initialState);
-    return (
-    <shipmentListContext.Provider value={{state,dispatch}}>
-    <ShipmentList />
-    </shipmentListContext.Provider>)
-}
 
-class ShipmentSide extends Component{
-  constructor(props){
-    super(props)
+const ShipmentData = () => {
+  const initialState = useContext(shipmentListContext);
+  const [state, dispatch] = useReducer(shipmentReducer, initialState);
+  return (
+    <shipmentListContext.Provider value={{ state, dispatch }}>
+      <ShipmentList />
+    </shipmentListContext.Provider>
+  );
+};
+
+class ShipmentSide extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-        collapse: false,
-      };
-      this.triggerCollapse = this.triggerCollapse.bind(this);
-        }
-    triggerCollapse(){
-        this.setState(state=> ({collapse:!state.collapse}))
-    }
-  
-  render(){
-    return(
-        <div>
-        <Card  onClick={this.triggerCollapse} style={styles.card}>
-        <CardBody>
-        <Row>
-          <Col xs="10" className="text-left">
-        <span style={styles.boxColor}>
-        <i className="fa fa-archive"></i></span>
-        <span style={styles.title}>Shipment Update : <span style={styles.status}>In Transit</span></span></Col>
-        <Col xs="2" className="text-right">
-        {this.state.collapse ? 
-       <span style={styles.arrow}><i className="fa fa-angle-down"></i></span>:
-        <span style={styles.arrow}><i className="fa fa-angle-right"></i></span>
-        }
-        </Col>
-        </Row>
-        <Collapse isOpen={this.state.collapse}>
-         <ShipmentData />
-          </Collapse>
-      </CardBody>
-   
-       </Card>
-       
+      collapse: false,
+    };
+    this.triggerCollapse = this.triggerCollapse.bind(this);
+  }
+
+  triggerCollapse() {
+    this.setState(state => ({ collapse: !state.collapse }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Card onClick={this.triggerCollapse} style={styles.card}>
+          <CardBody>
+            <Row>
+              <Col xs="10" className="text-left">
+                <span style={styles.boxColor}>
+                  <i className="fa fa-archive" />
+                </span>
+                <span style={styles.title}>
+                  Shipment Update :
+                  {' '}
+                  <span style={styles.status}>In Transit</span>
+                </span>
+              </Col>
+              <Col xs="2" className="text-right">
+                {this.state.collapse ? (
+                  <span style={styles.arrow}>
+                    <i className="fa fa-angle-down" />
+                  </span>
+                ) : (
+                  <span style={styles.arrow}>
+                    <i className="fa fa-angle-right" />
+                  </span>
+                )}
+              </Col>
+            </Row>
+            <Collapse isOpen={this.state.collapse}>
+              <ShipmentData />
+            </Collapse>
+          </CardBody>
+        </Card>
       </div>
-    )
+    );
   }
 }
 
 const styles = {
-   button:{ },
-   boxColor:{fontSize: 20, color: '#58CADB' ,marginRight:7},
-   title:{
-       fontSize: 20,
-       fontWeight: 'bold',
-       color: '#707070'
-   },
-   arrow:{
+  button: {},
+  boxColor: { fontSize: 20, color: '#58CADB', marginRight: 7 },
+  title: {
     fontSize: 20,
-    marginLeft:5,
-    color: '#707070'
-   },
-   status:{
-    color: '#58CADB'  
-   },
-   card:{
+    fontWeight: 'bold',
+    color: '#707070',
+  },
+  arrow: {
+    fontSize: 20,
+    marginLeft: 5,
+    color: '#707070',
+  },
+  status: {
+    color: '#58CADB',
+  },
+  card: {
     marginBottom: '0.2rem',
     marginTop: '0.2rem',
-    marginRight: '0.2rem'
-   }
-}
+    marginRight: '0.2rem',
+  },
+};
 
-export default ShipmentSide
+export default ShipmentSide;
