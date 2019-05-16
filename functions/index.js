a147af06da826f912da0b93058a2a70d2a470366
Thanongkiat Tamtai <thanongkiat.tamtai@gmail.com>
2019-05-16 10:25:52 +0700

    firebase

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-const from = require('rxjs');
+const firebase = require('firebase');
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
