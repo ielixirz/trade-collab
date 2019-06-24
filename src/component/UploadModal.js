a2fc71da4637161d9f3f85c0c0b165c89b236bd0
Rootrapee <r.rattanapipit@gmail.com>
2019-06-25 00:07:17 +0700

    fix: margin for cancel upload button

--- src/component/UploadModal.js
+++ src/component/UploadModal.js
@@ -254,7 +254,7 @@ Upload Files (
               <i
                 className="fa fa-times"
                 role="button"
-                style={{ cursor: 'pointer', marginLeft: '15px' }}
+                style={{ cursor: 'pointer', marginTop: '10px' }}
                 onClick={() => cancelUpload(index)}
                 onKeyDown={null}
                 tabIndex="-1"
