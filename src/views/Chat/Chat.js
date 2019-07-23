468fd342ef55a882eed2b455b880caef11355f3f
Punjasin Punya <punjasin@gmail.com>
2019-07-23 17:54:17 +0700

    fix move tab not work

--- src/views/Chat/Chat.js
+++ src/views/Chat/Chat.js
@@ -415,8 +415,16 @@ class Chat extends Component {
       });
     });
     tabs = _.sortBy(tabs, 'position');
-    let createChatIndex = _.find(tabs, (item, index) => item.ShipmentKey === 'custom');
-    tabs.push(tabs.splice(createChatIndex.id - 1, 1)[0]);
+    let itemToReplace = {};
+    let createChatIndex = _.find(tabs, (item, index) => {
+      if (item.ShipmentKey === 'custom') {
+        itemToReplace = tabs.splice(index, 1);
+        return true;
+      }
+      return false;
+    });
+    tabs = tabs.concat(itemToReplace);
+    console.log('Tabs list is', tabs);
     const activeTab = tabs.filter(tab => tab.active === true);
     const toggle = this.props.ChatReducer.toggle;
     const createChat = this.props.ChatReducer.createChat || false;
