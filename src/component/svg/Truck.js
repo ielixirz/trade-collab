09b99f4b8f8bd734f9e0ba2371ec4b51ca84003a
pattadon <ult3mate@gmail.com>
2019-08-14 13:46:03 +0700

    change ui shipment and chat

---
+++ src/component/svg/Truck.js
@@ -0,0 +1,59 @@
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+
+const Truck = () => (
+
+<svg xmlns="http://www.w3.org/2000/svg" width="21.999" height="15.252" viewBox="0 0 21.999 15.252">
+  <defs>
+  </defs>
+  <g id="delivery" transform="translate(0 0.001)">
+    <g id="Group_588" data-name="Group 588" transform="translate(14.832 10.379)">
+      <g id="Group_587" data-name="Group 587">
+        <path id="Path_709" data-name="Path 709" class="cls-1" d="M324.587,304.4a2.436,2.436,0,1,0,2.436,2.436A2.439,2.439,0,0,0,324.587,304.4Zm0,3.655a1.218,1.218,0,1,1,1.218-1.218A1.219,1.219,0,0,1,324.587,308.058Z" transform="translate(-322.151 -304.403)" />
+      </g>
+    </g>
+    <g id="Group_590" data-name="Group 590" transform="translate(4.546 10.379)">
+      <g id="Group_589" data-name="Group 589">
+        <path id="Path_710" data-name="Path 710" class="cls-1" d="M104.083,304.4a2.436,2.436,0,1,0,2.436,2.436A2.439,2.439,0,0,0,104.083,304.4Zm0,3.655a1.218,1.218,0,1,1,1.218-1.218A1.219,1.219,0,0,1,104.083,308.058Z" transform="translate(-101.647 -304.403)" />
+      </g>
+    </g>
+    <g id="Group_592" data-name="Group 592" transform="translate(15.157 1.218)">
+      <g id="Group_591" data-name="Group 591">
+        <path id="Path_711" data-name="Path 711" class="cls-1" d="M334.508,111.125a.609.609,0,0,0-.544-.335h-3.208v1.218h2.832l1.659,3.3,1.089-.547Z" transform="translate(-330.756 -110.79)" />
+      </g>
+    </g>
+    <g id="Group_594" data-name="Group 594" transform="translate(9.643 12.227)">
+      <g id="Group_593" data-name="Group 593">
+        <rect id="Rectangle_1484" data-name="Rectangle 1484" class="cls-1" width="4.609" height="1.218" />
+      </g>
+    </g>
+    <g id="Group_596" data-name="Group 596" transform="translate(1.726 12.227)">
+      <g id="Group_595" data-name="Group 595">
+        <path id="Path_712" data-name="Path 712" class="cls-1" d="M48.436,353.345H46.324a.609.609,0,0,0,0,1.218h2.112a.609.609,0,0,0,0-1.218Z" transform="translate(-45.715 -353.345)" />
+      </g>
+    </g>
+    <g id="Group_598" data-name="Group 598" transform="translate(1.726 0)">
+      <g id="Group_597" data-name="Group 597">
+        <path id="Path_713" data-name="Path 713" class="cls-1" d="M65.841,86.235l-1.38-1.79a.7.7,0,0,0-.554-.273h-5.1V79.227a.7.7,0,0,0-.7-.706H46.416a.706.706,0,0,0,0,1.413h10.99v4.945a.7.7,0,0,0,.7.706h5.456l1.022,1.325v3.643H62.645a.706.706,0,0,0,0,1.413h2.642a.7.7,0,0,0,.7-.706V86.668A.711.711,0,0,0,65.841,86.235Z" transform="translate(-45.715 -78.521)" />
+      </g>
+    </g>
+    <g id="Group_600" data-name="Group 600" transform="translate(0.995 8.397)">
+      <g id="Group_599" data-name="Group 599">
+        <path id="Path_714" data-name="Path 714" class="cls-1" d="M29.764,271.6h-2.8a.609.609,0,0,0,0,1.218h2.8a.609.609,0,0,0,0-1.218Z" transform="translate(-26.353 -271.597)" />
+      </g>
+    </g>
+    <g id="Group_602" data-name="Group 602" transform="translate(0 5.637)">
+      <g id="Group_601" data-name="Group 601">
+        <path id="Path_715" data-name="Path 715" class="cls-1" d="M5.807,208.134H.609a.609.609,0,0,0,0,1.218h5.2a.609.609,0,0,0,0-1.218Z" transform="translate(0 -208.134)" />
+      </g>
+    </g>
+    <g id="Group_604" data-name="Group 604" transform="translate(0.995 2.877)">
+      <g id="Group_603" data-name="Group 603">
+        <path id="Path_716" data-name="Path 716" class="cls-1" d="M32.16,144.672h-5.2a.609.609,0,0,0,0,1.218h5.2a.609.609,0,0,0,0-1.218Z" transform="translate(-26.353 -144.672)" />
+      </g>
+    </g>
+  </g>
+</svg>
+);
+
+export default Truck;
