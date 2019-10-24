0977735cb6f19513ba972332b1cffab7973bbb39
Punjasin Punya <punjasin@gmail.com>
2019-10-25 03:53:20 +0700

    order info component

--- src/views/Chat/components/OrderInfoTab.js
+++ src/views/Chat/components/OrderInfoTab.js
@@ -1,27 +1,95 @@
 import React, { Component } from 'react';
-import { Col, Row } from 'reactstrap';
+import { CardBody, Col, Form, FormGroup, Input, Label, Row } from 'reactstrap';
 
 class OrderInfoTab extends Component {
   render() {
     console.log('Data', this.props);
     return (
-      <div className="callout callout-info">
+      <div>
         <Row>
-          <Col md={1} />
-          <Col md={'auto'}>
-            <table className="table table-borderless">
-              <tbody>
-                <tr>
-                  <td>Port ETD : 02 Jan 2010</td>
-                  <td style={{ textAlign: 'right' }}>Otto</td>
-                </tr>
-                <tr>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                </tr>
-              </tbody>
-            </table>
-          </Col>
+          <div className="callout callout-info">
+            <Row>
+              <table className="table table-borderless">
+                <tbody
+                  style={{
+                    textAlign: 'right',
+                    fontSize: '0.9em',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  <tr>
+                    <td width={400}>
+                      Port ETD : 02 Jan 2010
+                      <br />
+                      <small>Port , Country</small>
+                    </td>
+                    <td width={200}>
+                      Quality Fresh
+                      <br />
+                      <small>Shipper</small>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </Row>
+          </div>
+        </Row>
+        <Row>
+          <div className="callout callout-danger">
+            <Row>
+              <table className="table table-borderless">
+                <tbody
+                  style={{
+                    textAlign: 'right',
+                    fontSize: '0.9em',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  <tr>
+                    <td width={400}>
+                      Port ETA : 02 Jan 2010
+                      <br />
+                      <small>Port , Country</small>
+                    </td>
+                    <td width={200}>
+                      Fluke Company
+                      <br />
+                      <small>Consignee</small>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td width={400}>
+                      Last free day : 02 Jan 2010
+                      <br />
+                      Est. Delivery : 02 Jan 2010
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </Row>
+          </div>
+        </Row>
+        <Row>
+          <Form
+            style={{
+              width: '100%',
+            }}
+          >
+            <FormGroup>
+              <Label htmlFor="Product">Product</Label>
+              <Input
+                type="text"
+                id="Product"
+                placeholder="Young Fresh Coconut"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="Details">
+                Details <i className="fa fa-lock fa-lg mt-4" />
+              </Label>
+              <Input type="text" id="Details" placeholder="Detail" />
+            </FormGroup>
+          </Form>
         </Row>
       </div>
     );
